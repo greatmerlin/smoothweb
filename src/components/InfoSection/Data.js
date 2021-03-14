d684a178ae9a2ed1b7f2fd1b1046d77014614253
Theo <gmbaxevanos@gmx.ch>
2021-03-14 21:36:04 +0100

    added second layer, still a bug with svg import to fix

---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,16 @@
+export const homeObjOne = {
+    id: 'about',
+    lightBg: false, 
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'MemoryUp',
+    headline: 'Earn $MEMO coins while training your memory',
+    description: 'Get access to the virtual gaming world and train your memory skills while receiving $MEMO coins. A fully decentralised game aiming to join the POLKADOT System. The game will be free to play while it is still in the beta version.',
+    buttonLabel: 'Get started',
+    imgStart: false,
+    img: require('../../images/svg-1.svg'),
+    alt: 'Cards',
+    dark: true,
+    primary: true,
+    darkText: false
+}
