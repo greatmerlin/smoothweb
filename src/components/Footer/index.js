167e932122ef9fc2fb73575b9c19c3bd760df296
Theo <gmbaxevanos@gmx.ch>
2021-03-15 22:05:37 +0100

    done with the footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,5 +1,27 @@
 import React from "react";
-import { FooterContainer,FooterWrap, FooterLinksContainer, FooterLinksWrapper, FooterLinkItems,FooterLinkTitel, FooterLink} from './FooterElements';
+import {
+  FooterContainer,
+  FooterWrap,
+  FooterLinksContainer,
+  FooterLinksWrapper,
+  FooterLinkItems,
+  FooterLinkTitel,
+  FooterLink,
+  SocialMedia,
+  SocialMediaWrap,
+  WebsiteRights,
+  SocialIcons,
+  SocialIconLink,
+  SocialLogo,
+} from "./FooterElements";
+
+import {
+  FaFacebook,
+  FaInstagram,
+  FaYoutube,
+  FaTwitter,
+  FaLinkedin,
+} from "react-icons/fa";
 
 const Footer = () => {
   return (
@@ -37,6 +59,35 @@ const Footer = () => {
             </FooterLinkItems>
           </FooterLinksWrapper>
         </FooterLinksContainer>
+        <SocialMedia>
+        <SocialMediaWrap>
+          <SocialLogo to="/">MemoUP</SocialLogo>
+          <WebsiteRights>
+            MemoryUP ® {new Date().getFullYear()} All rights reserved.
+          </WebsiteRights>
+          <SocialIcons>
+            <SocialIconLink href="/" target="_blank" aria-label="Facebook">
+              <FaFacebook />
+            </SocialIconLink>
+            <SocialIconLink href="/" target="_blank" aria-label="Instagram">
+              <FaInstagram />
+            </SocialIconLink>
+            <SocialIconLink href="/" target="_blank" aria-label="Youtube">
+              <FaYoutube />
+            </SocialIconLink>
+            <SocialIconLink
+              href="//www.twitter.com/merlindono"
+              target="_blank"
+              aria-label="Twitter"
+            >
+              <FaTwitter />
+            </SocialIconLink>
+            <SocialIconLink href="/" target="_blank" aria-label="Linkedin">
+              <FaLinkedin />
+            </SocialIconLink>
+          </SocialIcons>
+        </SocialMediaWrap>
+        </SocialMedia>
       </FooterWrap>
     </FooterContainer>
   );
